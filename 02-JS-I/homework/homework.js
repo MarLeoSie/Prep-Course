6f8d0d71d03b4b7c0877caa79a888ae1eeeaf27e
MarLeoSie <martinlsiele@gmail.com>
2022-04-21 22:00:02 -0300

    Homework finished JS I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -260,8 +260,7 @@ function esVocal(letra){
   }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
-  }
-    return "Es vocal"   
+  } 
 }
 
 
